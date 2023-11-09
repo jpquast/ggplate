38247dabecc7624aafa2ac2e5dbb1727aaf668af
jpquast <janphilipp.quast@gmail.com>
2023-11-09 14:37:33 +0100

    Fix bugs from issues

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# ggplate 0.1.0
+
+* Fixed a bug with the `value` argument that would not allow the user to provide a character column with only one value. This fixes issue #10.
+* Fixed a bug with the `label` argument that would discard trailing 0 of numeric values. This fixes issue #6.
+
 # ggplate 0.0.1
 
 * First release version.
--- R/plate_plot.R
+++ R/plate_plot.R
@@ -150,7 +150,7 @@ plate_plot <- function(data,
     # If there is only one numeric value in the data the colour function needs still two distinct values
     n_distinct_values <- length(unique(dplyr::pull(data, {{ value }})))
 
-    if (n_distinct_values == 1) {
+    if (n_distinct_values == 1 & is.numeric(min_val)) {
       max_val <- min_val + abs(min_val)
     }
   } else {
@@ -478,7 +478,14 @@ plate_plot <- function(data,
       y = ""
     ) +
     {
-      if (!missing(label)) ggplot2::geom_text(ggplot2::aes(x = col, y = .data$row_num, label = paste0({{ label }})), colour = data_prep$label_colours, size = label_size_scaled)
+      if (!missing(label)) ggplot2::geom_text(ggplot2::aes(x = col, 
+                                                           y = .data$row_num, 
+                                                           label = paste0(format(
+                                                             {{ label }}, 
+                                                             drop0Trailing = F)
+                                                             )), 
+                                              colour = data_prep$label_colours, 
+                                              size = label_size_scaled)
     } +
     ggplot2::theme_bw() +
     {
